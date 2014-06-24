b630dd6228770d287ae34fed33845a5e24edf848
Thomas Beeley <tbeeley@gmail.com>
2014-06-24 13:57:54 +0100

    chnage students to hash values

--- directory.rb
+++ directory.rb
@@ -1,39 +1,40 @@
 
 
-students =["Dave", 
-	"Eddie",
-	"Catharina",
-	"Igor", 
-	"Marco",
-	"Lisa",
-	"Michiel", 
-	"Jean", 
-	"Nicola", 
-	"Jennie", 
-	"Iona", 
-	"Nikesh", 
-	"Chloe", 
-	"Toan", 
-	"Jamie", 
-	"Peter", 
-	"Talal", 
-	"Charlie", 
-	"Charlotte",
-	"Thomas", 
-	"Zoe", 
-	"Hannah", 
-	"Joe", 
-	"Alex",
-	"Jeremy"]
-
+students =[
+	{:name => "Dave", :cohort=> :june},
+	{:name => "Eddie", :cohort =>:june},
+	{:name => "Catharina", :cohort =>:june},
+	{:name => "Igor", :cohort => :june},
+	{:name => "Marco", :cohort => :june},
+	{:name => "Lisa", :cohort=> :june},
+	{:name => "Michiel", :cohort =>:june},
+	{:name => "Jean", :cohort =>:june},
+	{:name => "Nicola", :cohort => :june},
+	{:name => "Jennie", :cohort => :june},
+	{:name => "Iona", :cohort=> :june},
+	{:name => "Nikesh", :cohort =>:june},
+	{:name => "Chloe", :cohort =>:june},
+	{:name => "Toan", :cohort => :june},
+	{:name => "Jamie", :cohort => :june},
+	{:name => "Peter", :cohort=> :june},
+	{:name => "Talal", :cohort =>:june},
+	{:name => "Charlie", :cohort =>:june},
+	{:name => "Charlotte", :cohort => :june},
+	{:name => "Thomas", :cohort => :june},
+	{:name => "Zoe", :cohort=> :june},
+	{:name => "Hannah", :cohort =>:june},
+	{:name => "Joe", :cohort =>:june},
+	{:name => "Alex", :cohort => :june},
+	{:name => "Jeremy", :cohort => :june}
+]
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts '-------------------'
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
 	end
 end
 
@@ -44,6 +45,7 @@ end
 print_header
 print(students)
 print_footer(students)
+
 
 
 
